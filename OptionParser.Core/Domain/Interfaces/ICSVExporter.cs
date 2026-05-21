bb33830a4aca9b39aac5e774c655263d2c84f657
Grigorii <griger1011@gmail.com>
2026-05-21 21:32:32 +0300

    chore: Initial commit, created base structure for domain

---
+++ OptionParser.Core/Domain/Interfaces/ICSVExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionParser.Core.Domain.Interfaces
+{
+    public interface ICSVExporter<T>
+    {
+        public void ExportToCSV(IEnumerable<T> parsedItems, string filename);
+    }
+}
---
+++ OptionParser.Core/Exporters/CSVExporter.cs
@@ -0,0 +1,17 @@
+﻿using OptionParser.Core.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionParser.Core.Exporters
+{
+    public class CSVExporter<T> : ICSVExporter<T>
+    {
+        public void ExportToCSV(IEnumerable<T> parsedItems, string filename) 
+        {
+            Console.WriteLine("Stub");
+        }
+    }
+}
