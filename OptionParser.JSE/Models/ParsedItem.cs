259bd45a7d76bbff01047915c4ae62252484755d
Grigorii <griger1011@gmail.com>
2026-05-26 13:04:53 +0300

    chore: added boilerplate code for jse parser

---
+++ OptionParser.JSE/Models/ParsedItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionParser.JSE.Models
+{
+    public record ParsedItem
+    {
+        public string ShortName { get; set; }
+        public string TradeDate { get; set; }
+        public string TradeType { get; set; }
+        public string FutureExpiry { get; set; }
+        public string Strike { get; set; }
+        public string CallPut { get; set; }
+        public string Quantity { get; set; }
+        public string Vol { get; set; }
+        public string Premium { get; set; }
+        public string FuturesPrice { get; set; }
+    }
+}
--- OptionParser.JSE/Program.cs
+++ OptionParser.JSE/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using log4net;
+using Microsoft.Extensions.DependencyInjection;
+using OptionParser.Core.Domain.Interfaces;
+using OptionParser.Core.Exporters;
+using OptionParser.JSE;
+using OptionParser.JSE.Models;
+using System.Numerics;
+using System.Reflection;
+
+string basePath = AppDomain.CurrentDomain.BaseDirectory;
+string configPath = Path.Combine(basePath, "logging.config");
+
+var start = DateTime.Now;
+
+var config = new FileInfo(configPath);
+
+log4net.Config.XmlConfigurator.Configure(config);
+
+ILog logger = LogManager.GetLogger("JSE");
+
+logger.Info($"Program launched at {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+var services = new ServiceCollection();
+
+services.AddHttpClient();
+
+services.AddSingleton<ILog>(logger);
+
+services.AddTransient<ICSVExporter<ParsedItem>, CSVExporter<ParsedItem>>();
+
+services.AddTransient<JSEParser>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var parser = serviceProvider.GetRequiredService<JSEParser>();
+
+Console.WriteLine("---------------------------------------------------------");
+
+Console.WriteLine("Логи сохраняются в директорию Logs в каталоге исполняемого файла.");
+
+Console.WriteLine($"Целевой сайт: {parser.site} [url: {JSEParser.SiteUrl}]");
+
+Console.Write("Введите директорию сохранения файлов (для сохранения в директорию исполняемого файла оставьте пустой): ");
+var outputDir = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(outputDir))
+{
+    outputDir = "./";
+}
+
+Console.WriteLine("Начало парсинга...");
+
+await parser.Run(outputDir);
+
+var totalTime = DateTime.Now - start;
+
+Console.WriteLine($"Затрачено времени: {totalTime:mm\\:ss}");
+Console.WriteLine("Парсинг завершен.");
+Console.WriteLine("---------------------------------------------------------");
+
+logger.Info($"Program finished at {DateTime.Now:dd.MM.yyyy HH:mm}");
